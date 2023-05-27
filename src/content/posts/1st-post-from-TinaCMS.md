7d1c5f839f4b4842b7948920086f66f800e6990a
16Bogdan54 <b.yunakov1654@gmail.com>
2023-05-27 22:10:29 +0300

    cms setup

---
+++ src/content/posts/1st-post-from-TinaCMS.md
@@ -0,0 +1,12 @@
+---
+title: 1st post from TinaCMS
+---
+
+## **Hello everyone!**
+
+> This is my very first experience with TinaCMS 
+
+1. List1
+2. List2 
+
+The End
---
+++ tina/config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "fd1703b9-c8b4-4b2d-beae-4b2b1c4c8374", // Get this from tina.io
+  token: "86f890f16ed1a6fe7494e6a7c9d886a92702233c", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "src/content/posts",
+        format: 'md',
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
