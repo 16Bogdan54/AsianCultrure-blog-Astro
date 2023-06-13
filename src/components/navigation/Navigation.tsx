ef0dd946bcd7b0c6bd5759307a0c391d43c66f58
16Bogdan54 <b.yunakov1654@gmail.com>
2023-06-13 23:06:36 +0300

    nav refactor, theme toggler

---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,23 @@
+import type React from "react";
+import { Navbar } from "flowbite-react";
+import ThemeToggle from "../themeToggle/ThemeToggle";
+const Navigation: React.FC = () => {
+  return (
+    <Navbar fluid rounded>
+      <Navbar.Brand href="/">
+        <img src="/logo.svg" alt="logo" />
+      </Navbar.Brand>
+      <div className="flex md:order-2">
+        <ThemeToggle />
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link href="/korea">Korea</Navbar.Link>
+        <Navbar.Link href="/china">China</Navbar.Link>
+        <Navbar.Link href="/japan">Japan</Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Navigation;
