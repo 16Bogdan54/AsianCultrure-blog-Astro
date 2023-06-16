7407fad1d74a07b93e45970b141b1fcf464b02d4
16Bogdan54 <b.yunakov1654@gmail.com>
2023-06-16 14:02:16 +0300

    plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,7 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
-    require("flowbite/plugin")
+    require("flowbite/plugin"),
+    require("@tailwindcss/line-clamp")
   ],
 };
