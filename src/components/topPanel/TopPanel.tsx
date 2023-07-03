cb7915235ec743d483f6f8cd8f43579d03d12308
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-07-03 15:57:55 +0300

    fix dropdown

--- src/components/topPanel/TopPanel.tsx
+++ src/components/topPanel/TopPanel.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import BlogPost from "../blogpost/BlogPost";
+import TagList from "../tagList/TagList";
 import Fuse from "fuse.js";
 
 interface Props {
@@ -39,61 +40,7 @@ const TopPanel = ({ title, tags, searchList }: Props) => {
         </h2>
         <div className="flex-grow">
           <div className="flex">
-            <label
-              htmlFor="search-dropdown"
-              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-            >
-              Your Email
-            </label>
-            <button
-              id="dropdown-button"
-              data-dropdown-toggle="dropdown"
-              className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 dark:border-gray-700 dark:text-white rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-              type="button"
-            >
-              All Tags
-              <svg
-                aria-hidden="true"
-                className="w-4 h-4 ml-1"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
-            <div
-              id="dropdown"
-              className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-            >
-              <ul
-                className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                aria-labelledby="dropdown-button"
-              >
-                {tags.map((tag: string) => (
-                  <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                    <div className="flex items-center pl-3">
-                      <input
-                        id={tag}
-                        type="checkbox"
-                        value={tag}
-                        className="w-4 h-4 text-primary bg-gray-100 border-gray-300 rounded focus:ring-primary dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        htmlFor={tag}
-                        className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                      >
-                        {tag}
-                      </label>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
+            <TagList tags={tags} />
             <div className="relative w-full">
               <input
                 type="search"
